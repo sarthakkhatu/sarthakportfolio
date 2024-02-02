2a5001ab7c5471d4ac8c0cff3c170534f08d2d84
sarthakkhatu <140178907+sarthakkhatu@users.noreply.github.com>
2024-02-03 00:56:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 2, // Adjust the typing speed (higher value = slower)
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
